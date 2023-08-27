e410a5b18c929ea0db411438a3585d3f2b170adb
CDemmenie <cdemmenie@gmail.com>
2023-08-27 14:55:45 +0100

    Fixed history snscrape exception.

--- crawler/history.py
+++ crawler/history.py
@@ -58,24 +58,29 @@ class history():
                       f"from {channel}")
                 posts = telegram.TelegramChannelScraper(channel).get_items()
 
-            except snscrape.base.ScraperException as err:
-                print(f"[{datetime.datetime.now()}] Caught: {err}",
-                        "continuing.")
-                continue
+                for post in posts:
+                    
+                    #First checking for montioned accounts that we might not have
+                    #stored yet.
+                    self.lists = main().findTelUsers(post.outlinks, self.lists)
 
-            for post in posts:
-                
-                #First checking for montioned accounts that we might not have
-                #stored yet.
-                self.lists = main().findTelUsers(post.outlinks, self.lists)
-
-                #Now looking to see if the post has a video in it.
-                try:
+                    #Now looking to see if the post has a video in it.
                     lastSlash = post.url.rfind('/')
                     postID = post.url[lastSlash+1:]
                     url = f"https://t.me/{channel}/{postID}"
+                    
+                    try:
+                        hashHex, hashDec = main().videoHash(url)
 
-                    hashHex, hashDec = main().videoHash(url)
+                    except videohash.exceptions.DownloadFailed as err:
+                        print(f"[{datetime.datetime.now()}] Caught: {err}",
+                            "continuing.")
+                        continue
+
+                    except videohash.exceptions.FFmpegFailedToExtractFrames as err:
+                        print(f"[{datetime.datetime.now()}] Caught: {err}",
+                            "continuing.")
+                        continue
 
                     postOb = self.postOb()
                     postOb.hashDec = hashDec
@@ -88,16 +93,11 @@ class history():
                     
                     main().resolve(postOb, hashHex, hashDec)
 
+            except snscrape.base.ScraperException as err:
+            print(f"[{datetime.datetime.now()}] Caught: {err}",
+                    "continuing.")
+            continue
 
-                except videohash.exceptions.DownloadFailed as err:
-                    print(f"[{datetime.datetime.now()}] Caught: {err}",
-                        "continuing.")
-                    continue
-
-                except videohash.exceptions.FFmpegFailedToExtractFrames as err:
-                    print(f"[{datetime.datetime.now()}] Caught: {err}",
-                        "continuing.")
-                    continue
 
 if __name__ == "__main__":
 
