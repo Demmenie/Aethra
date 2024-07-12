76f559b87977a53b8fe7f7210c360299562d6901
CDemmenie <11903978+Demmenie@users.noreply.github.com>
2024-07-12 18:38:04 +0100

    Removed redundant file removal.

--- crawler/maintenance.py
+++ crawler/maintenance.py
@@ -227,14 +227,6 @@ class maintenance:
 
         #print(self.videoHashHex, self.videoHashDec)
 
-        videoPath = vHash.storage_path
-        cutPath = videoPath[:videoPath.find("temp_storage_dir")]
-
-        try:
-            shutil.rmtree(cutPath)
-        except OSError as e:
-            print("Error: %s - %s." % (e.filename, e.strerror))
-
 
     def moveList(self):
 
