7fe6d398ecd3b40b6725308e974d7ae719bada89
CDemmenie <11903978+Demmenie@users.noreply.github.com>
2024-07-13 21:33:22 +0100

    Fixing empty cutPath error in maintenance.

--- crawler/maintenance.py
+++ crawler/maintenance.py
@@ -228,12 +228,13 @@ class maintenance:
         #print(self.videoHashHex, self.videoHashDec)
 
         videoPath = vHash.storage_path
-        cutPath = videoPath[:videoPath.find("/temp_storage_dir")]
+        cutPath = list(videoPath)[:videoPath.find("/temp_storage_dir")]
 
         try:
-            shutil.rmtree(cutPath)
+            shutil.rmtree(cutPath.join(""))
         except OSError as e:
             print("Error: %s - %s." % (e.filename, e.strerror))
+            print(cutPath)
 
 
     def moveList(self):
